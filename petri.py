8886500f504133be0d3d28a13e7e185a7da5d747
DDOtten <daniel@otten.co>
2019-09-12 15:10:52 +0200

    manier om pijlen te verwijderen en een simpele debug print

--- petri.py
+++ petri.py
@@ -11,6 +11,15 @@ class Place:
 		self.x = x
 		self.y = y
 
+	def __str__(self):
+		return "(name: " + str(self.name) \
+			+ ", tokens: " + str(self.tokens) \
+			+ ", input: " + str(self.input) \
+			+ ", output: " + str(self.output) \
+			+ ")"
+
+	__repr__ = __str__
+
 class Transition:
 	def __init__(self, name, x, y):
 		self.name = name
@@ -19,12 +28,28 @@ class Transition:
 		self.x = x
 		self.y = y
 
+	def __str__(self):
+		return "(name: " + str(self.name) \
+			+ ", input: " + str(self.input) \
+			+ ", output: " + str(self.output) \
+			+ ")"
+
+	__repr__ = __str__
+
+
 class PetriNet:
 	def __init__(self):
 		self.nextPlaceId = 0
 		self.nextTransitionId = 0
 		self.places = {}
 		self.transitions = {}
+
+	def __str__(self):
+		return "(places: " + str(self.places) \
+			+ ", transitions: " + str(self.transitions) \
+			+ ")"
+
+	__repr__ = __str__
 
 	def addPlace(self, name, x, y):
 		id = self.nextPlaceId
@@ -33,8 +58,8 @@ class PetriNet:
 		return id
 
 	def addTransition(self, name, x, y):
-		id = self.nextPlaceId
-		elf.transitions[id] = Transition(name, x, y)
+		id = self.nextTransitionId
+		self.transitions[id] = Transition(name, x, y)
 		self.nextTransitionId += 1
 		return id
 
@@ -45,12 +70,36 @@ class PetriNet:
 		del self.transitions[id]
 
 	def addArrowPlaceToTransition(self, placeId, transitionId):
-		self.places[placeId].output += [transitionId]
-		self.transitions[transitionId].input += [placeId]
+		output = self.places[placeId].output
+		input = self.transitions[transitionId].input
+		if transitionId not in output:
+			output.append(transitionId)
+		if placeId not in input:
+			input.append(placeId)
 
 	def addArrowTransitionToPlace(self, transitionId, placeId):
-		self.places[placeId].output += [transitionId]
-		self.transitions[transitionId].input += [placeId]
+		output = self.transitions[transitionId].output
+		input = self.places[placeId].input
+		if placeId not in output:
+			output.append(placeId)
+		if transitionId not in input:
+			input.append(transitionId)
+
+	def removeArrowPlaceToTransition(self, placeId, transitionId):
+		output = self.places[placeId].output
+		input = self.transitions[transitionId].input
+		if transitionId in output:
+			output.remove(transitionId)
+		if placeId in input:
+			input.remove(placeId)
+
+	def removeArrowTransitionToPlace(self, placeId, transitionId):
+		output = self.transitions[transitionId].output
+		input = self.places[placeId].input
+		if placeId in output:
+			output.remove(placeId)
+		if transitionId in input:
+			input.remove(transitionId)
 
 
 class Project:
