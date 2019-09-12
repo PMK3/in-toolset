e19d587e5cc84ea95aa7ea9d25383343861c64de
DDOtten <daniel@otten.co>
2019-09-12 14:16:04 +0200

    veranderd naar dictionary

--- petri.py
+++ petri.py
@@ -3,7 +3,8 @@ from signal import Signal
 
 
 class Place:
-	def __init__(self, x, y):
+	def __init__(self, name, x, y):
+		self.name = name
 		self.tokens = 0
 		self.input = []
 		self.output = []
@@ -11,7 +12,8 @@ class Place:
 		self.y = y
 
 class Transition:
-	def __init__(self, x, y):
+	def __init__(self, name, x, y):
+		self.name = name
 		self.input = []
 		self.output = []
 		self.x = x
@@ -19,47 +21,55 @@ class Transition:
 
 class PetriNet:
 	def __init__(self):
-		self.places = []
-		self.transitions = []
+		self.nextPlaceId = 0
+		self.nextTransitionId = 0
+		self.nextPlaceName = "A"
+		self.nextTransitionName = "a"
+		self.places = {}
+		self.transitions = {}
 
 	def addPlace(self, x, y):
-		self.places.append(Place(x, y))
+		self.places[self.nextPlaceId] = Place(self.nextPlaceName, x, y))
+		self.nextPlaceId += 1
+		self.nextPlaceName += 1
 
 	def addTransition(self, x, y):
-		self.transitions.append(Transition(x, y))
+		self.transitions[self.nextTransitionId].append(Transition(self.nextTransitionName, x, y))
+		self.nextTransitionId += 1
+		self.nextTransitionName += 1
 
 	def addArrowPlaceToTransition(self, placeId, transitionId):
 		self.places[placeId].output += [transitionId]
-		self.transitions[transitionId].input +=[placeId]
+		self.transitions[transitionId].input += [placeId]
 
 	def addArrowTransitionToPlace(self, transitionId, placeId):
 		self.places[placeId].output += [transitionId]
 		self.transitions[transitionId].input += [placeId]
 
-		
+
 class Project:
 	def __init__(self):
 		self.filenameChanged = Signal()
 		self.unsavedChanged = Signal()
-		
+
 		self.filename = None
 		self.unsaved = False
-		
+
 	def setFilename(self, filename):
 		if self.filename != filename:
 			self.filename = filename
 			self.filenameChanged.emit()
-			
+
 	def setUnsaved(self, unsaved):
 		if self.unsaved != unsaved:
 			self.unsaved = unsaved
 			self.unsavedChanged.emit()
-		
+
 	def load(self, filename):
 		self.setFilename(filename)
 		self.setUnsaved(False)
 		#TODO: Load petri net from file
-		
+
 	def save(self, filename):
 		self.setFilename(filename)
 		self.setUnsaved(False)
