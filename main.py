3b547dd924c05b7296e12b337280c6e4fe3d7d5d
Jakob Wuhrer <jakobwuhrer@airmail.cc>
2019-09-15 00:49:25 +0200

    remove obsolete testing code

--- main.py
+++ main.py
@@ -3,16 +3,5 @@ from ui.app import Application
 from petri import PetriNet
 
 if __name__ == "__main__":
-	# testing
-	#net = PetriNet()
-	#print(net)
-	#net.addArrowPlaceToTransition(net.addPlace("p", 1, 2), net.addTransition("t", 2, 1))
-	#print(net)
-	#net.removeArrowPlaceToTransition(0, 0)
-	#print(net)
-	#net.removePlace(0)
-	#net.removeTransition(0)
-	#print(net)
-
 	app = Application()
 	app.start()
