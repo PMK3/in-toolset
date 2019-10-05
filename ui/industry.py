c9f390640c542fc72254a598774acec271bef48e
DDOtten <daniel@otten.co>
2019-10-05 14:22:00 +0200

    changed enterprise to diamond

--- ui/industry.py
+++ ui/industry.py
@@ -10,49 +10,49 @@ class EnterpriseItem(ActiveNode):
 class IndustryController:
 	def __init__(self, style, window):
 		self.style = style
-		
+
 		self.toolbar = window.toolbar
 		self.scene = window.scene
-		
+
 		self.net = None
-	
+
 	def load(self, net):
 		self.net = net
-		
+
 	def startPlacement(self, pos):
 		type = self.toolbar.currentTool("industry")
 		if type == "enterprise":
 			self.scene.setHoverEnabled(False)
-			item = EditorNode(self.scene, self.style.shapes["place"])
+			item = EditorNode(self.scene, self.style.shapes["enterprise"])
 			item.drag(pos)
 			return item
-		
+
 	def finishPlacement(self, pos, item):
 		if isinstance(item, EditorNode):
 			pos = alignToGrid(pos)
 			x, y = pos.x(), pos.y()
-			
+
 			if not item.invalid:
 				enterprise = EnterpriseNode(x, y)
 				self.net.enterprises.add(enterprise)
-		
+
 		self.scene.setHoverEnabled(True)
-		
-		
+
+
 class GeneralSettings(QWidget):
 	def __init__(self, net):
 		super().__init__()
 		self.setStyleSheet("font-size: 16px")
-		
+
 		self.net = net
-		
+
 		self.label = QLabel("No item selected")
 		self.label.setAlignment(Qt.AlignCenter)
 
 		self.layout = QVBoxLayout(self)
 		self.layout.addWidget(self.label)
 		self.layout.setAlignment(Qt.AlignTop)
-		
+
 	def cleanup(self):
 		pass
 
@@ -63,8 +63,8 @@ class GeneralSettings(QWidget):
 			self.label.setText("1 item selected")
 		else:
 			self.label.setText("%i items selected" %len(items))
-			
-			
+
+
 class EnterpriseSettings(QWidget):
 	def __init__(self, obj):
 		super().__init__()
@@ -86,7 +86,7 @@ class EnterpriseSettings(QWidget):
 		self.layout.addRow("X:", self.x)
 		self.layout.addRow("Y:", self.y)
 		self.layout.addRow("Label:", self.label)
-		
+
 	def cleanup(self):
 		self.obj.positionChanged.disconnect(self.updatePos)
 		self.obj.labelChanged.disconnect(self.updateLabel)
@@ -102,71 +102,71 @@ class EnterpriseSettings(QWidget):
 class IndustryScene:
 	def __init__(self, style, window):
 		self.style = style
-		
+
 		self.window = window
-		
+
 		self.controller = IndustryController(style, window)
-		
+
 		self.toolbar = window.toolbar
 		self.scene = window.scene
 		self.view = window.view
 		self.settings = window.settings
-		
+
 	def load(self, net):
 		self.scene.clear()
-		
+
 		self.net = net
 		self.net.enterprises.added.connect(self.addEnterprise)
-		
+
 		for enterprise in self.net.enterprises:
 			self.addEnterprise(enterprise)
-			
+
 		self.controller.load(net)
-		
+
 		self.scene.setController(self.controller)
 		self.scene.selectionChanged.connect(self.updateSelection)
-		
+
 		self.view.setHandDrag(False)
-		
+
 		self.toolbar.reset()
 		self.toolbar.addGroup("common")
 		self.toolbar.addGroup("industry")
 		self.toolbar.selectTool("selection")
 		self.toolbar.selectionChanged.connect(self.updateTool)
-		
+
 		self.generalSettings = GeneralSettings(self.net)
 		self.settings.setWidget(self.generalSettings)
-		
+
 	def cleanup(self):
 		self.scene.selectionChanged.disconnect(self.updateSelection)
 		self.toolbar.selectionChanged.disconnect(self.updateTool)
-		
+
 		self.net.enterprises.added.disconnect(self.addEnterprise)
-		
+
 		self.generalSettings.cleanup()
 		if self.settings.widget() != self.generalSettings:
 			self.settings.widget().cleanup()
 		self.scene.cleanup()
-		
+
 	def addEnterprise(self, obj):
-		item = EnterpriseItem(self.scene, self.style.shapes["place"], obj)
+		item = EnterpriseItem(self.scene, self.style.shapes["enterprise"], obj)
 		self.scene.addItem(item)
-		
+
 	def updateSelection(self):
 		if self.settings.widget() != self.generalSettings:
 			self.settings.widget().cleanup()
-		
+
 		items = self.scene.selectedItems()
 		widget = self.createSettingsWidget(items)
 		self.settings.setWidget(widget)
-		
+
 	def createSettingsWidget(self, items):
 		if len(items) == 1 and isinstance(items[0], EnterpriseItem):
 			return EnterpriseSettings(items[0].obj)
-		
+
 		self.generalSettings.setSelection(items)
 		return self.generalSettings
-		
+
 	def updateTool(self, tool):
 		if tool == "selection":
 			self.view.setHandDrag(False)
