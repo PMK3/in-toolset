f06d0fe2e62fd0b5af1e912eb1c158a29b312b90
Jakob Wuhrer <jakobwuhrer@airmail.cc>
2019-10-21 22:35:14 +0200

    Implement basic messaging features in backend

--- petri/industry.py
+++ petri/industry.py
@@ -1,5 +1,5 @@
 
-from petri.base import ObjectList, Node
+from petri.base import ObjectList, Node, Object
 from petri.petri import PetriNet, Transition
 from common import Signal, Property
 import random
@@ -43,6 +43,7 @@ class TransitionType:
 
 class EnterpriseTransition(Transition):
 	type = Property("typeChanged", TransitionType.INTERNAL)
+	message = Property("messageChanged", None)
 	messageType = Property("messageTypeChanged", "")
 	arrowAngle = Property("arrowChanged", math.pi)
 	industryAngle = Property("industryArrowChanged", math.pi)
@@ -51,6 +52,7 @@ class EnterpriseTransition(Transition):
 		super().__init__(x, y)
 		self.typeChanged = Signal()
 		self.messageTypeChanged = Signal()
+		self.messageChanged = Signal()
 		self.arrowChanged = Signal()
 		self.industryArrowChanged = Signal()
 		
@@ -58,8 +60,23 @@ class EnterpriseTransition(Transition):
 	def setMessageType(self, messageType): self.messageType = messageType
 	def setArrowAngle(self, angle): self.arrowAngle = angle
 	def setIndustryAngle(self, angle): self.industryAngle = angle
+	def setMessage(self, message): self.message = message
+
+	def pushMessageType(self):
+		if self.message:
+			if self.type == TransitionType.INPUT and self.message.output:
+				self.message.output.messageType = self.messageType
+			if self.type == TransitionType.OUTPUT and self.message.input:
+				self.message.input.messageType = self.messageType
 
 
+class Message(Object):
+	def __init__(self, input, output):
+		super().__init__()
+		self.input = input
+		self.output = output
+		self.input.deleted.connect(self.delete)
+		self.output.deleted.connect(self.delete)
 
 class EnterpriseNode(Node):
 	def __init__(self, x, y):
@@ -79,3 +96,28 @@ class IndustryNet:
 		
 		self.enterprises.changed.connect(self.changed)
 		self.messages.changed.connect(self.changed)
+
+	def canConnect(self, input, output):
+		if not (input.type == TransitionType.INPUT and output.type == TransitionType.OUTPUT):
+			return False
+		if not input.messageType == output.messageType:
+			return False
+		if input.net == output.net:
+			return False
+		return True
+
+	def connect(self, input, output):
+		if not self.canConnect(input, output):
+			raise ValueError("Invalid transitions")
+		message = Message(input, output)
+
+		input.message = message
+		output.message = message
+
+		input.messageTypeChanged.connect(lambda: message.active and input.pushMessageType())
+		output.messageTypeChanged.connect(lambda: message.active and output.pushMessageType())
+
+		message.deleted.connect(lambda: message.output.setMessage(None))
+		message.deleted.connect(lambda: message.input.setMessage(None))
+
+		self.messages.add(message)
