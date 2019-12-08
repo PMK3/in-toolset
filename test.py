ff1ef90506d419f11c4e6b4d9831718f6c290b47
Julia <julia.bolt71@gmail.com>
2019-12-08 16:25:55 +0100

    een paar unittests erbij

--- test.py
+++ test.py
@@ -9,7 +9,78 @@ if version < (3, 6):
 import unittest
 from model.base import *
 
+
+class TestPlace(unittest.TestCase):
+
+    def testSetTokens(self):
+        net = PetriNet()
+        net.places.add(Place())
+        net.places[0].setTokens(9001)
+        self.assertTrue(net.places[0].tokens > 9000)
+        self.assertFalse(net.places[0].tokens > 9001)
+
+    def testGive(self):
+        net = PetriNet()
+        net.places.add(Place())
+        net.places[0].give()
+        self.assertTrue(net.places[0].tokens == 1)
+        net.places[0].give()
+        self.assertTrue(net.places[0].tokens == 2)
+
+    def testTake(self):
+        net = PetriNet()
+        net.places.add(Place())
+        net.places[0].setTokens(2)
+        self.assertTrue(net.places[0].tokens == 2)
+        net.places[0].take()
+        self.assertFalse(net.places[0].tokens > 1)
+        self.assertTrue(net.places[0].tokens > 0)
+
+
+class TestTransition(unittest.TestCase):
+
+    def testCheckEnabled(self):
+        net = PetriNet()
+        net.transitions.add(Transition())
+        self.assertTrue(net.transitions[0].checkEnabled())
+        net.places.add(Place())
+        net.places[0].postset.add(net.transitions[0])
+        net.transitions[0].preset.add(net.places[0])
+        self.assertFalse(net.transitions[0].checkEnabled())
+        net.places[0].give()
+        self.assertTrue(net.transitions[0].checkEnabled())
+
+    def testUpdateEnabled(self):
+        net = PetriNet()
+        net.transitions.add(Transition())
+        net.places.add(Place())
+        net.places[0].postset.add(net.transitions[0])
+        net.transitions[0].preset.add(net.places[0])
+        net.transitions[0].updateEnabled()
+        self.assertFalse(net.transitions[0].enabled)
+        net.places[0].give()
+        net.transitions[0].updateEnabled()
+        self.assertTrue(net.transitions[0].enabled)
+
+    def testTrigger(self):
+        net = PetriNet()
+        net.transitions.add(Transition())
+        net.places.add(Place())
+        net.places[0].postset.add(net.transitions[0])
+        net.transitions[0].preset.add(net.places[0]) #place 0 points to transition 0
+        net.places.add(Place())
+        net.transitions[0].postset.add(net.places[1])
+        net.places[1].preset.add(net.transitions[0]) #transition 0 points to place 1
+        net.places[0].give()
+        self.assertTrue(net.places[0].tokens == 1)
+        self.assertTrue(net.places[1].tokens == 0)
+        net.transitions[0].trigger()
+        self.assertTrue(net.places[0].tokens == 0)
+        self.assertTrue(net.places[1].tokens == 1)
+
+
 class TestPetriNet(unittest.TestCase):
+
     def testEnabledTransitions(self):
         net = PetriNet()
         self.assertTrue(len(net.enabledTransitions()) == 0)
@@ -48,29 +119,16 @@ class TestPetriNet(unittest.TestCase):
         net.triggerRandom()
         self.assertTrue(net.places[0].tokens == 0)
 
-    def testSetTokens(self):
+    def testSetInitialMarking(self):
         net = PetriNet()
         net.places.add(Place())
-        net.places[0].setTokens(9001)
-        self.assertTrue(net.places[0].tokens > 9000)
-        self.assertFalse(net.places[0].tokens > 9001)
-
-    def testGive(self):
-        net = PetriNet()
         net.places.add(Place())
-        net.places[0].give()
+        self.assertTrue(net.places[0].tokens == 0)
+        self.assertTrue(net.places[1].tokens == 0)
+        net.setInitialMarking()
         self.assertTrue(net.places[0].tokens == 1)
-        net.places[0].give()
-        self.assertTrue(net.places[0].tokens == 2)
+        self.assertTrue(net.places[1].tokens == 1)
 
-    def testTake(self):
-        net = PetriNet()
-        net.places.add(Place())
-        net.places[0].setTokens(2)
-        self.assertTrue(net.places[0].tokens == 2)
-        net.places[0].take()
-        self.assertFalse(net.places[0].tokens > 1)
-        self.assertTrue(net.places[0].tokens > 0)
 
 
 if __name__ == '__main__':
