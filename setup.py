145393e254376b1c056f43f478d804fe45d39fcc
Jakob Wuhrer <jakobwuhrer@airmail.cc>
2019-12-10 18:35:56 +0100

    increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="in-toolset",
-	version="0.0.15",
+	version="0.1.0",
 	author="Example Author",
 	author_email="author@example.com",
 	description="An editor and simulator for industry workflow nets (inets), a model of interorganisational workflows based on petrinets",
